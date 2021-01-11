53b2d1c7f3a0188c7cdcd83ebbe56587f94341a3
remote-torrent-6747 <abbyc@sky.com>
2021-01-11 20:01:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,32 @@
 class Song
 
-def initialize(name, artist, genre)
-  @name = name
-  @artist = artist
-  @genre = genre
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def name= (name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def artist= (artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist
+  end
+
+  def genre= (genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
 end
