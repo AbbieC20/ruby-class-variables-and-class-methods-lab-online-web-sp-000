8279599515f771f39a79e57777f0e8cc74e40978
remote-torrent-6747 <abbyc@sky.com>
2021-01-11 20:11:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,14 @@ class Song
   @@count = 0
   @@artists = 0
   @@genres = 0
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@artists += 1
+    @@genres += 1
   end
 
   def self.count
